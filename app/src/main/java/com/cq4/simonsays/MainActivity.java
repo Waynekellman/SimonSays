066ab78e82d99faec0ab80bab648860eb0ded143
Melina Gonzalez <melinag1992@gmail.com>
2017-10-30 22:04:20 -0400

    first commit

---
+++ app/src/main/java/com/cq4/simonsays/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cq4.simonsays;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
