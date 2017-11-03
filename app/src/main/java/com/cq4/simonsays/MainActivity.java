7ce5df92aaa4882c26c56aa4bc324a2f918f4da6
Melina Gonzalez <melinag1992@gmail.com>
2017-11-02 20:42:43 -0400

    Design commit

--- app/src/main/java/com/cq4/simonsays/MainActivity.java
+++ app/src/main/java/com/cq4/simonsays/MainActivity.java
@@ -1,7 +1,14 @@
 package com.cq4.simonsays;
 
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView myTextView = (TextView) findViewById(R.id.textView);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/bromo.otf");
+        myTextView.setTypeface(typeface);
+
+
+
     }
+
+
 }
